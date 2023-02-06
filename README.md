2c15058fc0422b1dc86ac8dbc84c832a866af4f5
armaanjaj <armaan.jaj@gmail.com>
2023-02-06 08:07:33 -0700

    add search button to table list and improve user experience

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ The project of purpose is to show case my skills for full stack application deve
 * UI-tools: CSS3 (Vanilla CSS)
 * HTTP request architecture: RestAPI
 * IDE : Visual Studio Code
+
+# Modules used
+* React:
+  * @mui/icons-material
+  * axios
+  * http-proxy-middleware
+  * react-router-dom
+  * redux
+  * redux-thunk
+  * universal-cookie
+ 
+* Node:
+  * express
+  * body-parser
+  * nodemon
+  
+# Some snips from development phase
+* <img width="960" alt="Screenshot_20230206_075940" src="https://user-images.githubusercontent.com/113659942/217007151-152bc1df-a041-4751-a9b3-3df9654a4f95.png">
+* ![Screenshot_20230206-075849](https://user-images.githubusercontent.com/113659942/217007200-672809d7-7ac1-433c-acd1-f5b1b1cfebdc.png)
--- client/src/Pages/Inventory/Inventory.js
+++ client/src/Pages/Inventory/Inventory.js
@@ -1,22 +1,148 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../../Components/Footer/Footer";
 import CancelIcon from "@mui/icons-material/Cancel";
 import EditIcon from "@mui/icons-material/Edit";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
 import "./Inventory.css";
 import { useSelector } from "react-redux";
 
 function Inventory() {
+    const mode = useSelector((state) => state.darkMode);
+    const [searchBox, setSearchBox] = useState(false);
 
-    const mode = useSelector(state => state.darkMode);
+    const inventoryArray = [
+        {
+            category: "Kitchen",
+            item: "Fan",
+            price: 15.99,
+        },
+        {
+            category: "Bedroom",
+            item: "Clock",
+            price: 30.0,
+        },
+        {
+            category: "Garage",
+            item: "Stand",
+            price: 9.99,
+        },
+        {
+            category: "Living room",
+            item: "TV",
+            price: 1099.99,
+        },
+        {
+            category: "Office",
+            item: "Table",
+            price: 50.0,
+        },
+        {
+            category: "Kitchen",
+            item: "Fan",
+            price: 15.99,
+        },
+        {
+            category: "Bedroom",
+            item: "Clock",
+            price: 30.0,
+        },
+        {
+            category: "Garage",
+            item: "Stand",
+            price: 9.99,
+        },
+        {
+            category: "Living room",
+            item: "TV",
+            price: 1099.99,
+        },
+        {
+            category: "Office",
+            item: "Table",
+            price: 50.0,
+        },
+        {
+            category: "Kitchen",
+            item: "Fan",
+            price: 15.99,
+        },
+        {
+            category: "Bedroom",
+            item: "Clock",
+            price: 30.0,
+        },
+        {
+            category: "Garage",
+            item: "Stand",
+            price: 9.99,
+        },
+        {
+            category: "Living room",
+            item: "TV",
+            price: 1099.99,
+        },
+        {
+            category: "Office",
+            item: "Table",
+            price: 50.0,
+        },
+    ];
 
     return (
         <>
             <div className="inventory-body-main">
                 <div className="inventory-body-content">
                     <div className="inventory-body-list">
-                        <div className="inventory-body-list-left" style={{backgroundColor:`${mode.bgCard}`, color:`${mode.colorCard}`}}>
+                        <div
+                            className="inventory-body-list-left"
+                            style={{
+                                backgroundColor: `${mode.bgCard}`,
+                                color: `${mode.colorCard}`,
+                            }}
+                        >
                             <div className="inventory-table-head">
-                                Your inventory items
+                                <div>
+                                    {inventoryArray.length > 0
+                                        ? `You have ${inventoryArray.length} ${
+                                              inventoryArray.length > 1
+                                                  ? "items"
+                                                  : "item"
+                                          } in your inventory`
+                                        : "You don't have any items yet"}
+                                </div>
+                                <div className="inventory-body-searchbox">
+                                    {searchBox ? (
+                                        <div>
+                                            <input
+                                                type="text"
+                                                className="field search-item"
+                                                name="itemSearch"
+                                                placeholder="Search item"
+                                                style={{
+                                                    backgroundColor: `${mode.bgCard}`,
+                                                    color: `${mode.colorCard}`,
+                                                }}
+                                            />
+                                        </div>
+                                    ) : (
+                                        ""
+                                    )}
+                                    <span
+                                        onClick={() => {
+                                            setSearchBox(!searchBox);
+                                        }}
+                                        className="inventory-body-searchbox"
+                                    >
+                                        {searchBox ? (
+                                            <>
+                                                <CloseIcon />
+                                            </>
+                                        ) : (
+                                            <SearchIcon />
+                                        )}
+                                    </span>
+                                </div>
                             </div>
                             <table className="table">
                                 <tbody>
@@ -24,41 +150,61 @@ function Inventory() {
                                         <th>Category</th>
                                         <th>Name</th>
                                         <th>Price</th>
-                                        <th>Actions</th>
+                                        <th colSpan={2}>Actions</th>
                                     </tr>
-                                    <tr style={{ textAlign: "left" }}>
-                                        <td>Category</td>
-                                        <td>Name</td>
-                                        <td>Price</td>
-                                        <td>
-                                            <button
-                                                type="submit"
-                                                className="inventory-body-list-action btn-edit"
-                                            >
-                                                <EditIcon />
-                                            </button>
-                                            <button
-                                                type="submit"
-                                                className="inventory-body-list-action btn-delete"
-                                            >
-                                                <CancelIcon />
-                                            </button>
-                                        </td>
-                                    </tr>
+                                    {inventoryArray.map((item) => (
+                                        <tr
+                                            key={item}
+                                            style={{ textAlign: "left" }}
+                                        >
+                                            <td>{item.category}</td>
+                                            <td>{item.item}</td>
+                                            <td>{item.price}</td>
+                                            <td>
+                                                <button
+                                                    type="submit"
+                                                    className="inventory-body-list-action btn-edit"
+                                                >
+                                                    <EditIcon />
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button
+                                                    type="submit"
+                                                    className="inventory-body-list-action btn-delete"
+                                                >
+                                                    <CancelIcon />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
                         <div className="inventory-body-list-right">
-                            <div className="inventory-body-forms" style={{backgroundColor:`${mode.bgCard}`, color:`${mode.colorCard}`}}>
+                            <div
+                                className="inventory-body-forms"
+                                style={{
+                                    backgroundColor: `${mode.bgCard}`,
+                                    color: `${mode.colorCard}`,
+                                }}
+                            >
                                 <h2>Add Item</h2>
                                 <form className="inventory-body-item-container-form">
                                     <div className="inventory-body-item-form-fieldDiv">
                                         <select
                                             className="field"
                                             name="category"
-                                            style={{backgroundColor:`${mode.bgCard}`, color:`${mode.colorCard}`}}
+                                            style={{
+                                                backgroundColor: `${mode.bgCard}`,
+                                                color: `${mode.colorCard}`,
+                                            }}
                                         >
-                                            <option disabled selected value={null}>
+                                            <option
+                                                disabled
+                                                selected
+                                                value={null}
+                                            >
                                                 -Select Category-
                                             </option>
                                             <option value={null}>Name</option>
@@ -70,7 +216,10 @@ function Inventory() {
                                             className="field"
                                             name="itemName"
                                             placeholder="Item name"
-                                            style={{backgroundColor:`${mode.bgCard}`, color:`${mode.colorCard}`}}
+                                            style={{
+                                                backgroundColor: `${mode.bgCard}`,
+                                                color: `${mode.colorCard}`,
+                                            }}
                                         />
                                     </div>
                                     <div className="warning">
@@ -82,7 +231,10 @@ function Inventory() {
                                             className="field"
                                             name="price"
                                             placeholder="Item price"
-                                            style={{backgroundColor:`${mode.bgCard}`, color:`${mode.colorCard}`}}
+                                            style={{
+                                                backgroundColor: `${mode.bgCard}`,
+                                                color: `${mode.colorCard}`,
+                                            }}
                                         />
                                     </div>
                                     <div className="warning">
@@ -102,7 +254,7 @@ function Inventory() {
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 }
